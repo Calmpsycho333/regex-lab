9f32f9ad27c0daa31cff4acdd9b0a24471fb3c91
calmpsycho333 <wrathking75@yahoo.com>
2020-12-26 12:06:43 -0500

    commit tree 8187a1f42548d20ce134ef393f0673ac84ad1523

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,44 @@
-def starts_with_a_vowel?(word)
 
+require 'pry' 
+def starts_with_a_vowel?(word)
+   #binding.pry
+ if word.match(/\b[aeiouAEIOU]/)
+    return true
+ #elsif word.match(/[aeiou]/)
+    #return true
+ else
+
+   return false 
+
+ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    counts = []
+    count = text.split
+   # binding.pry
+   count.each do | words | 
+    counts << words  if words.match(/\b(un)\S+(ing)/) 
+   end
+   return counts
 end
 
 def words_five_letters_long(text)
-
+empty = []
+array = text.split
+array.each do | words |
+    empty << words if words.match(/^\w{5}$/)
+end
+return empty
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+empty = []
+array = text
+array.each do | words | 
+    empty << words if words.match(/\A\p{Lu}.*\p{P}\z/)
+end
+return empty
 end
 
 def valid_phone_number?(phone)
